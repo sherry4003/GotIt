01777140295ebe88980a975a597d1d43da1108ca
sherry4003 <bdang4@gmu.edu>
2016-11-14 10:50:46 -0500

    validation

--- app/src/main/java/com/example/gotit/SetActivity.java
+++ app/src/main/java/com/example/gotit/SetActivity.java
@@ -1,9 +1,12 @@
 package com.example.gotit;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class SetActivity extends AppCompatActivity implements View.OnClickListener{
     Button submitButton;
@@ -21,6 +24,25 @@ public class SetActivity extends AppCompatActivity implements View.OnClickListen
         submitButton = (Button) findViewById(R.id.submitButton);
         submitButton.setOnClickListener(this);
     }
+    
+        //validayion
+    private boolean validation(){
+        message_from_edit_text =message.getText().toString();
+        start = starttime.getText().toString();
+        end = endtime.getText().toString();
+
+        if (message_from_edit_text.isEmpty() || start.isEmpty() || end.isEmpty()){
+            showDialog();
+            return false;
+        }
+
+        if (monday.isChecked() || tuesday.isChecked() || wednesday.isChecked() || thursday.isChecked()|| friday.isChecked()||saturday.isChecked()||sunday.isChecked()){
+            return true;
+        }
+
+        return false;
+    }
+    
 
     @Override
     public void onClick(View v) {
@@ -30,4 +52,22 @@ public class SetActivity extends AppCompatActivity implements View.OnClickListen
                 break;
         }
     }
+    
+        //show the message error
+    public void showDialog(){
+        AlertDialog alertDialog = new AlertDialog.Builder(SetActivity.this).create();
+        alertDialog.setTitle("Missing field");
+        alertDialog.setMessage("Please enter all fields");
+        // Alert dialog button
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Alert dialog action goes here
+                        // onClick button code here
+                        dialog.dismiss();// use dismiss to cancel alert dialog
+                    }
+                });
+        alertDialog.show();
+    }
+
 }
